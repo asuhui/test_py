fbcf72f30f1ee933f4ae4e81c8591c95a07b8bee
sha <18726003966@163.com>
2019-07-15 14:29:21 +0800

    add my define exception

---
+++ sh_exception_myself.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+class PasswordException(Exception):
+    def __init__(self,name,pwd):
+        self.name=name
+        self.pwd=pwd
+    def __str__(self):
+        return ("密码%s长度不正确。"%(self.pwd))
+def reg(uname,pwd):
+    if len(pwd)<6:
+        raise PasswordException(uname.pwd)
+    else:
+        print("用户名为：%s,密码为%s"%(uname,pwd))
+try:
+    reg("su","123")
+except Exception as e:
+    print(e)
+
