9e153e4f0d319f46e70f8bf202973b06fb7b1b3a
sha <18726003966@163.com>
2019-07-25 16:34:01 +0800

    modified sh_staticmethod.py

--- sh_staticmethod.py
+++ sh_staticmethod.py
@@ -1,11 +1,12 @@
 #coding=utf-8
 class Person(object):
+    name="su"#类属性
     def __init__(self,name):
         self.name=name
     @staticmethod
-    def getName(self):
+    def getName(self):#静态方法
         return self.name
 if __name__=="__main__":
-    p=Person("sh")
-    print(p.getName())
+    
+    print(Person.getName())
     
