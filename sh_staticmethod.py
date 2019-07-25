7e2dca9fdfbc86365695f704df443a1c6a674bd6
sha <18726003966@163.com>
2019-07-25 16:17:24 +0800

    add staticmethod content

--- sh_staticmethod.py
+++ sh_staticmethod.py
@@ -1,1 +1,11 @@
 #coding=utf-8
+class Person(object):
+    def __init__(self,name):
+        self.name=name
+    @staticmethod
+    def getName(self):
+        return self.name
+if __name__=="__main__":
+    p=Person("sh")
+    print(p.getName())
+    
